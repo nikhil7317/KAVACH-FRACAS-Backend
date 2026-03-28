d9a0e4836b070dc9e9df53763c26eae4ca01d388
Nikhil Gupta <79450273+nikhil7317@users.noreply.github.com>
2026-03-28 12:01:28 +0530

    latest commit

--- src/main/java/com/railbit/tcasanalysis/service/StationQueryService.java
+++ src/main/java/com/railbit/tcasanalysis/service/StationQueryService.java
@@ -22,14 +22,6 @@ public class StationQueryService {
     @Autowired
     private StationaryPacketRepository stationaryPacketRepository;
 
-    /**
-     * Find station packets with filters - returns latest 100 records.
-     *
-     * @param fromDate required - start date/time
-     * @param toDate   required - end date/time
-     * @param stnCode  optional - filter by SOURCE_STN_ILC_IBS_ID
-     * @param locoId   optional - filter by DEST_LOCO_ID from inner packet
-     */
     public List<StationaryPacket> findPackets(Date fromDate, Date toDate, Integer stnCode, Integer locoId) {
 
         logger.info("Query: from={}, to={}, stnCode={}, locoId={}", fromDate, toDate, stnCode, locoId);
